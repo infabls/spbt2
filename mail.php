93a971395b5809849647ef1a48eee2c5dc2b8faf
Infabls <infabls.com@gmail.com>
2018-05-28 15:46:37 +0600

    hfgh

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ if (!empty($_POST['phone'])) {
 	$email = clean_data($_POST['email']);
 	$message = "Посетитель $name заказал обратный звонок на номер $phone. Почта $email"; // Формируем сообщение, отправляемое на почту
 	$to = 'bragin_german@mail.ru'; // Задаем получателя письма
-	$from = $phone;  // От кого пришло письмо
+	$from = 'mail@vostokspbt.kz';  // От кого пришло письмо
 	$subject = "Заявка на ГБО с сайта vostokspbt.kz"; // Задаем тему письма
 	$headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
 	mail($to, $subject, $message, $headers);
