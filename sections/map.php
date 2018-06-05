df6bfb92e63a412f291b45aa9ff0435a6f9fce78
Infabls <infabls.com@gmail.com>
2018-06-05 18:34:16 +0600

    map data update

--- sections/map.php
+++ sections/map.php
@@ -1,6 +1,8 @@
 <div id="map">
 	<div class="col-md-10 col-md-offset-1">	
-		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7c35e553570538c221695267fb15d6a8b72d07d0fb43ee30cdad06f20f60667a&amp;width=100%&amp;height=483&amp;lang=ru_RU&amp;scroll=true"></script>
+		<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8dd3d605c259b3b280489a233a2b4f347139bbc17307fbe12a77eadb9a364ab4&amp;width=100%&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+
+
 	</div>
 <style>
 	#contact {
