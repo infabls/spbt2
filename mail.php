6435a27ca88d3fd3bc9409a8329657dff9adb0fe
Infabls <infabls.com@gmail.com>
2018-06-05 17:48:57 +0600

    mail php

--- mail.php
+++ mail.php
@@ -6,14 +6,9 @@ if (!empty($_POST['phone'])) {
 	$email = clean_data($_POST['email']);
 	$message = "Посетитель " . $name . " заказал обратный звонок на номер " . $phone . " .Почта " . $email; // Формируем сообщение, отправляемое на почту
 	$to = 'bragin_german@mail.ru'; // Задаем получателя письма
-	$to2 = 'Anuarmanabaev@mail.ru'; // Задаем получателя письма
-	$to3 = 'Imashev.rinat@mail.ru'; // Задаем получателя письма
-	$from = 'mail@vostokspbt.kz';  // От кого пришло письмо
 	$subject = "Заявка на ГБО с сайта vostokspbt.kz"; // Задаем тему письма
 	$headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
 	mail($to, $subject, $message, $headers);
-	mail($to2, $subject, $message, $headers);
-	mail($to3, $subject, $message, $headers);
 }
 
 function clean_data ($data){
@@ -22,5 +17,7 @@ function clean_data ($data){
 	$data = htmlspecialchars($data);
 	return $data;
 }
+header('Location: /');
+exit;
 
  ?>
